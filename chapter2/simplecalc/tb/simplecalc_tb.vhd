814ec47e26396c87c37f309bde55c694d0ceb9c9
Fabian DIRLINGER (HWSW participant) <m12326119@ti7.tilab.tuwien.ac.at>
2024-12-04 18:48:02 +0100

    AA

--- chapter2/simplecalc/tb/simplecalc_tb.vhd
+++ chapter2/simplecalc/tb/simplecalc_tb.vhd
@@ -8,9 +8,41 @@ end entity;
 
 architecture bench of simplecalc_tb is
 
+	
+	constant DATA_WIDTH : natural := 8;
+	
+	signal clk    : std_ulogic;
+	signal res_n  : std_ulogic := '1';
+
+	signal operand_data_in : std_ulogic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
+	signal store_operand1  : std_ulogic := '1';
+	signal store_operand2  : std_ulogic := '1';
+
+	signal sub : std_ulogic := '0'; 
+
+	signal operand1 : std_ulogic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
+	signal operand2 : std_ulogic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
+	signal result   : std_ulogic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
+
 begin
 
 	-- Instantiate the unit under test
+
+	uut : entity work.simplecalc
+	generic map (
+		DATA_WIDTH => DATA_WIDTH
+	)
+	port map (
+		clk => clk,
+		res_n => res_n,
+		operand_data_in => operand_data_in,
+		store_operand1 => store_operand1,
+		store_operand2 => store_operand2,
+		sub => sub,
+		operand1 => operand1,
+		operand2 => operand2,
+		result => result
+	);
 
 	-- Stimulus process
 	stimulus: process
@@ -18,14 +50,32 @@ begin
 		report "simulation start";
 
 		-- Apply test stimuli
+		
+		wait for 15 ns;
 
-
-		wait for 10 ns;
-		assert 1 = 0 report "Test x failed" severity error;
+		operand_data_in <= std_logic_vector(to_unsigned(13, DATA_WIDTH));
+		store_operand1 <= '0';
+		wait for 30 ns;
+		store_operand1 <= '1';
+		operand_data_in <= std_logic_vector(to_unsigned(17, DATA_WIDTH));
+		store_operand2 <= '0';
+		wait for 30 ns;
+		store_operand2 <= '1';
+		
+		report "Result: " & to_string(unsigned(result));
 
 		report "simulation end";
 		-- End simulation
 		wait;
 	end process;
+
+	clk_p : process
+	begin
+		clk <= '0';
+		wait for 5 ns;
+		clk <= '1';
+		wait for 5 ns;
+	end process;
+
 end architecture;
 
--- chapter2/simplecalc/top_arch.vhd
+++ chapter2/simplecalc/top_arch.vhd
@@ -5,5 +5,34 @@ use ieee.numeric_std.all;
 use work.util_pkg.all;
 
 architecture top_arch_simplecalc of top is
+	constant DATA_WIDTH : natural := 8;
+	signal operand1 : std_ulogic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
+	signal operand2 : std_ulogic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
+	signal result   : std_ulogic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
 begin
+	
+	/*
+	uut : entity work.simplecalc
+	generic map(
+		DATA_WIDTH => DATA_WIDTH
+	)
+	port map (
+		clk => clk,
+		res_n => keys(0),
+		operand_data_in => switches(7 downto 0),
+		sub => switches(17),
+		store_operand1 => keys(1),
+		store_operand2 => keys(2),
+		operand1 => operand1,
+		operand2 => operand2,
+		result => result
+	);
+	*/
+	operand1 <= "01000110";
+	hex7 <= to_segs(operand1(7 downto 4));
+	hex6 <= to_segs(operand1(3 downto 0));
+	hex5 <= to_segs(operand2(7 downto 4));
+	hex4 <= to_segs(operand2(3 downto 0));
+	hex3 <= to_segs(result(7 downto 4));
+	hex2 <= to_segs(result(3 downto 0));
 end architecture;
